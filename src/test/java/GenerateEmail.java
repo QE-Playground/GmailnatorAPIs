19300b052d47fe8b5f67722ef7fe749387bb7393
DANG Truong <dangtruong080593@gmail.com>
2021-01-20 22:04:16 +0700

    Integrate with Extent Report

--- src/test/java/GenerateEmail.java
+++ src/test/java/GenerateEmail.java
@@ -7,20 +7,42 @@ import static org.junit.Assert.assertThat;
 
 import gmailnator.Utils;
 import okhttp3.*;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 import java.io.IOException;
 import java.util.List;
 
 public class GenerateEmail {
 
-    OkHttpClient client;
+    private OkHttpClient client;
+
+    private ExtentReports report;
+
+    private ExtentTest test;
+
+    private ExtentHtmlReporter htmlReporter;
 
     @Before
     public void init() {
 
         client = new OkHttpClient();
+        report = new ExtentReports();
+        htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/test-output/STMExtentReport.html");
+
+        report.attachReporter(htmlReporter);
+        htmlReporter.config().setDocumentTitle("Gmailnator's REST APIs");
+        htmlReporter.config().setReportName("report gmailnator");
+        htmlReporter.config().setTheme(Theme.STANDARD);
+
+        test = report.createTest("generateEmail", "Generate a temporary email by gmailnator's REST APIs");
+
     }
 
     @Test
@@ -34,6 +56,8 @@ public class GenerateEmail {
         Response response = call.execute();
 
         assertThat(response.code(), equalTo(ResponseCode.OK));
+
+        test.log(Status.PASS, "Access to gmailnator's home page successfully.");
 
         List<String> cookies = response.headers().values(CookieHeaders.SET_COOKIE);
         String csrfGmailnatorToken = Utils.getCsrfGmailnatorCookie(cookies);
@@ -54,8 +78,17 @@ public class GenerateEmail {
 
         assertThat(generateEmailResponse.code(), equalTo(ResponseCode.OK));
 
+        test.log(Status.PASS, "Temporary email is generated successfully.");
+
         String generatedEmail = generateEmailResponse.body().string();
 
         System.out.println("Generated email from Gmailnator: " + generatedEmail);
+    }
+
+    @After
+    public void tearDown() {
+
+        report.flush();
+
     }
 }
